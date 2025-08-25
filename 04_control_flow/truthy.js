ddbc2e9ecd589ac30fea1bc0a2d12c563505e729
Mayuri Agrawal <52383823+Mayuri-Agrawal17@users.noreply.github.com>
2025-08-25 10:32:18 +0000

    truthy value

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,27 @@
+/*🟢 What are Truthy and Falsy Values?
+
+👉 In JavaScript, when a value is used in a boolean context (like if(condition)),
+JavaScript automatically converts it to either true or false.
+
+If the value becomes false → it is Falsy.
+If the value becomes true → it is Truthy.
+
+
+🟢------------------ Falsy Values in JavaScript
+
+There are only 7 falsy values in JS 👇
+
+false
+0 (zero)
+-0 (negative zero)
+0n (BigInt zero)
+"" (empty string)
+null
+undefined
+NaN
+
+----⚡ Any value other than these is truthy.
+special to remember:
+1.a non-empty string → truthy. even " " is also truthy also,"0" "false" also truthy
+2.⚡ Even though they look empty, [] and {} are truthy. 
+3.function(){} empty function is also truthy*/
